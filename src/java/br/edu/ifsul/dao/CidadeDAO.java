7fa4f45d7d8a3d297dd23dd8ce745607a16d969e
20171pf.cc0178 <20171pf.cc0178@pc-90463.passofundo.ifsul.local>
2019-05-13 11:39:58 -0300

    Criação do módulo cidade

--- src/java/br/edu/ifsul/dao/CidadeDAO.java
+++ src/java/br/edu/ifsul/dao/CidadeDAO.java
@@ -1,21 +1,27 @@
-package br.edu.ifsul.dao; 
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifsul.dao;
 
 import br.edu.ifsul.converters.ConverterOrdem;
-import javax.ejb.Stateful;
+import br.edu.ifsul.modelo.Cidade;
 import br.edu.ifsul.modelo.Estado;
 import java.io.Serializable;
+import javax.ejb.Stateful;
 
 @Stateful
-public class EstadoDAO<TipoGenerics> extends DAOGenerico<Estado> implements Serializable {
+public class CidadeDAO<TipoGenerics> extends DAOGenerico<Cidade> implements Serializable {
     
-    public EstadoDAO(){
-        super(Estado.class);
+    public CidadeDAO(){
+        super(Cidade.class);
         // classePersistente = Estado.class;
         // inicializar as ordenações possiveis        
         listaOrdem.add(new Ordem("id", "ID", "="));
         listaOrdem.add(new Ordem("nome", "Nome", "like"));
         // definir qual a ordenação padrão no caso o segundo elemento da lista (indice 1)
-        ordemAtual = listaOrdem.get(0);
+        ordemAtual = listaOrdem.get(1);
         // inicializar o conversor com a lista de ordens
         converterOrdem = new ConverterOrdem(listaOrdem);
     }
